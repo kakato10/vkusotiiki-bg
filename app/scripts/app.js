87f667682eb51cce574c7250960ed924ed3fe929
kakato10 <svetimir.ignatov@gmail.com>
2016-04-30 20:33:12 +0300

    Initializing the app and it's structure

---
+++ app/scripts/app.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name vkusotiikiBgApp
+ * @description
+ * # vkusotiikiBgApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('vkusotiikiBgApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'ui.router'
+  ])
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise("/");
+    $stateProvider
+      .state('home', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main',
+        url: '/'
+      })
+      .state('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about',
+        url: '/about'
+      });
+
+  });
---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name vkusotiikiBgApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the vkusotiikiBgApp
+ */
+angular.module('vkusotiikiBgApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
