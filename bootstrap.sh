1477ae2e3adbcfbbac1810337827ce83a148abb7
Milka Ferezliyska <mileto94@abv.bg>
2016-05-01 01:07:31 +0300

    Add some settings for vagrant and guide notes

---
+++ bootstrap.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+set -e
+
+color() {
+      printf '\033[%sm%s\033[m\n' "$@"
+      # usage color "31;5" "string"
+      # 0 default
+      # 5 blink, 1 strong, 4 underlined
+      # fg: 31 red,  32 green, 33 yellow, 34 blue, 35 purple, 36 cyan, 37 white
+      # bg: 40 black, 41 red, 44 blue, 45 purple
+      }
+
+color '36;1' "
+
+     This script installs dependencies for Vkusotiiki-bg.
+
+     For more details, visit:
+     https://github.com/kakato10/vkusotiiki-bg
+"
+
+color '35;1' 'Updating packages...'
+apt-get update
+
+color '35;1' 'Installing dependencies from apt-get...'
+apt-get -y install git \
+                   build-essential \
+                   ruby-full \
+                   nodejs \
+                   npm
+
+color '35;1' 'Finished installing dependencies...'
+
+color '35;1' 'Cleaning up...'
+apt-get -y autoremove
+
+color '35;1' 'Install compass gem...'
+sudo gem install compass
+
+color '35;1' 'Installinng npm packages...'
+sudo npm install n -g
+sudo n stable
+sudo npm install -g grunt-cli bower yo generator-karma generator-angular
+
+color '35;1' 'Install libs with bower...'
+bower install
+color '35;1' 'Finish.'
+npm install
+
+color '35;1' 'Done!.'
---
+++ notes.md
@@ -0,0 +1,33 @@
+# How to set up working environment locally?
+
+### Download and install
+ - latest version of [Virtual box](https://www.virtualbox.org/wiki/Downloads)
+ - latest version of [Vagrant](https://www.vagrantup.com/downloads.html)
+
+### Clone repo locally and set up environment
+```sh
+$ git clone https://github.com/kakato10/vkusotiiki-bg.git
+```
+
+ - move to project working directory (OS X):
+```sh
+$ cd vkusotiiki-bg
+```
+
+ - open terminal / command line and type:
+```sh
+$ vagrant up
+```
+ - WAIT (do something else for about 30-60min). Last command will create a new virtual machine without GUI and will install all needed packages inside of it
+ - After all finished, enter in the terminal /  command line:
+```sh
+$ vagrant ssh
+```
+
+### Now you're in the game!
+Type
+```sh
+$ cd vkusotiiki-bg
+$ grunt serve
+```
+## Voilà!
