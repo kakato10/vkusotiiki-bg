2fa5e32db389c2d42ee5f2f7fbcfefe1c73cde2b
Milka Ferezliyska <mileto94@abv.bg>
2016-06-04 03:16:41 +0300

    Dummy

--- web.js
+++ web.js
@@ -1,13 +1,8 @@
-// server.js
 var gzippo = require('gzippo');
-var express = require("express");
-var logfmt = require("logfmt");
+var express = require('express');
+var morgan = require('morgan');
 var app = express();
 
-app.use(logfmt.requestLogger());
-
+app.use(morgan('dev'));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
-var port = Number(process.env.PORT || 5000);
-app.listen(port, function() {
-console.log("Listening on " + port);
-});
+app.listen(process.env.PORT || 5000);
