75da8e709e52781ec319064c8aeea5603fce3351
Milka Ferezliyska <mileto94@abv.bg>
2016-06-04 03:10:49 +0300

    Change lib in server file

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
+// server.js
 var gzippo = require('gzippo');
-var express = require('express');
-var morgan = require('morgan');
+var express = require("express");
+var logfmt = require("logfmt");
 var app = express();
 
-app.use(morgan('dev'));
+app.use(logfmt.requestLogger());
+
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
-app.listen(process.env.PORT || 5000);
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function() {
+console.log("Listening on " + port);
+});
