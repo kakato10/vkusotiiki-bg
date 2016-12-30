6e858cc447157e509b14869bd8ac039bfd51995e
Salih Kadiev <Salih Kadiev>
2016-12-30 21:28:44 +0200

    refoctor styles

--- php_services/ServerConfig.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Define connection parameters
-    $server_name = "127.0.0.1";
-    // $server_name = "192.168.33.1";
-    $username = "root";
-    $password = "root";
-    $db_name = "vkusotiiki-bg";
-?>
